fc9e08e23bd5fb8d235298de38a05bc7b6d02538
David Lukes <dafydd.lukes@gmail.com>
2015-11-20 12:58:25 +0100

    improve update.sh
    - make sure that:
      - submodules are pushed as well
      - local changes are re-played on the source tree after submodule updates

--- update.sh
+++ update.sh
@@ -4,6 +4,6 @@ git pull
 # first do a recursive update of all submodules to pull in changes approved by third-party repositories
 git submodule update --recursive
 # then update toplevel submodules to the newest remote version
-git submodule update --remote
+git submodule update --remote --rebase
 git commit -am "update submodules"
-git push
+git push --recurse-submodules=on-demand
