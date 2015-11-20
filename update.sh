615b3bb0760fa864cf26a988c9437632dfb289b7
David Lukes <dafydd.lukes@gmail.com>
2015-11-20 12:48:05 +0100

    improve update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 
 git pull
-git submodule update --remote
+# first do a recursive update of all submodules to pull in changes approved by third-party repositories
 git submodule update --recursive
-git commit -a
+# then update toplevel submodules to the newest remote version
+git submodule update --remote
+git commit -am "update submodules"
