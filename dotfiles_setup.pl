7a28258cd11cb1a0c406b7ed0fec0975ec3a2a96
David Lukes <dafydd.lukes@gmail.com>
2018-01-31 11:13:07 +0100

    Refactor setup script

--- dotfiles_setup.pl
+++ dotfiles_setup.pl
@@ -5,6 +5,7 @@ use strict;
 use warnings;
 use FindBin;
 use File::Basename;
+use File::Path;
 
 system "git submodule update --init --recursive"
   and die "Couldn't update/init submodules: $!";
@@ -12,27 +13,31 @@ system "git submodule update --init --recursive"
 my $dotdir = $FindBin::RealBin.'/';
 my $home = $ENV{'HOME'}.'/';
 my @dotfiles = grep { !/
-  ^.git(modules|ignore)?$
- |^\.config$
- |^\.{1,2}$
+  \/.git(modules|ignore)?$
+ |\/\.config$
+ |\/\.{1,2}$
  |disabled
  |\.DS_Store
-  /x } glob '.* texmf .config/*';
+  /x } glob $dotdir.'{.*,texmf,.config/*}';
 
-for my $dotfile (@dotfiles) {
-  # check for pre-existing links/files/dirs
-  if (-l $home.$dotfile) {
-    unlink $home.$dotfile or die "Unable to remove existing symlink: $!";
-  } elsif (-e $home.$dotfile) {
-    print STDERR "WARNING: a non-symlink file named $dotfile already exists "
-      ."in $home. Symlink not created.\n";
-    next;
+create_symlinks($dotdir, $home, @dotfiles);
+create_symlinks($dotdir.'snippets/', $dotdir.'.emacs.d/private/snippets/', glob $dotdir.'snippets/*');
+
+sub create_symlinks {
+  my $from = shift;
+  my $to = shift;
+  my @targets = @_;
+  for my $target (@targets) {
+    $target = basename $target;
+    # check for pre-existing links/files/dirs
+    if (-l $to.$target) {
+      unlink $to.$target or die "Unable to remove existing symlink: $!";
+    } elsif (-e $to.$target) {
+      print STDERR "WARNING: a non-symlink file named $target already exists "
+          ."in $to. Symlink not created.\n";
+      next;
+    }
+    print STDERR "Symlinking $from$target to $to$target.\n";
+    symlink $from.$target, $to.$target or die "Symlinking failed: $!";
   }
-  print STDERR "Symlinking $dotdir$dotfile to $home$dotfile.\n";
-  symlink $dotdir.$dotfile, $home.$dotfile or die "Symlinking failed: $!";
-}
-
-for my $snippet_dir (glob $dotdir."snippets/*") {
-  print STDERR "Symlinking $snippet_dir to $dotdir.emacs.d/private/snippets/$snippet_dir.\n";
-  symlink $snippet_dir, $dotdir.".emacs.d/private/snippets/".(basename $snippet_dir) or die "Symlinking failed: $!";
 }
