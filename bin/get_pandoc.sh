3b07b3eed7b9bcff20f0172b8674245b624ad412
David Lukes <dafydd.lukes@gmail.com>
2018-04-18 12:06:28 +0200

    Add script for getting latest pandoc binary release

---
+++ bin/get_pandoc.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env zsh
+
+set -e
+
+tmpdir=`mktemp -d`
+cd $tmpdir
+
+>&2 echo "Getting download links and archive names..."
+pandoc_link=$(
+  curl -sL https://github.com/jgm/pandoc/releases/latest |
+    grep -oPm 1 '/jgm/.*?/pandoc.*?-linux\.tar\.gz'
+           )
+crossref_link=$(
+  curl -sL https://github.com/lierdakil/pandoc-crossref/releases/latest |
+    grep -oPm 1 '/lierdakil/.*?/linux.*?\.tar\.gz'
+             )
+pandoc_archive_name=`basename $pandoc_link`
+crossref_archive_name=`basename $crossref_link`
+
+>&2 echo "Fetching and extracting archives..."
+wget -q https://github.com$pandoc_link
+wget -q https://github.com$crossref_link
+tar xzf $pandoc_archive_name
+tar xzf $crossref_archive_name
+
+>&2 echo "Removing archives..."
+rm -f $pandoc_archive_name
+rm -f $crossref_archive_name
+
+>&2 echo "Merging pandoc-crossref into pandoc's directory tree..."
+mv pandoc-crossref pandoc-*/bin
+mv pandoc-crossref.1 pandoc-*/share/man/man1
+
+>&2 echo "Moving everything under '$HOME/.local...'"
+local_tree=$HOME/.local
+mkdir -p $local_tree/bin
+mkdir -p $local_tree/share/man/man1
+mv pandoc-* $local_tree
+
+>&2 echo "Creating symlinks..."
+cp -rs $local_tree/pandoc-*/* $local_tree/
+
+>&2 echo "You should now be able to run pandoc, pandoc-citeproc and pandoc-crossref."
+>&2 echo "If it doesn't work, make sure that:"
+>&2 echo "  - '$local_tree/bin' is on your PATH"
+>&2 echo "  - '$local_tree/share/man' is on your MANPATH"
