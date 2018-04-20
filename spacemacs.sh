81eae1d4fb4295c7e394a964ebb9eab6eb966d44
David Lukes <dafydd.lukes@gmail.com>
2018-04-20 18:34:17 +0200

    Fix dead-key-related Emacs input bug

--- spacemacs.sh
+++ spacemacs.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-exec env TMPDIR="/tmp/`whoami`" emacsclient -ca emacs $@
+# unsetting XMODIFIERS gets rid of "<dead_tilde> is undefined" bug
+exec env TMPDIR="/tmp/`whoami`" XMODIFIERS= emacsclient -ca emacs $@
