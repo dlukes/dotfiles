c5679e4a3ed7dfc37fa87f4cce11b33781eea24f
David Lukes <dafydd.lukes@gmail.com>
2019-07-04 12:51:02 +0200

    Remove old unused dotfiles
    If I ever need any one of those again, I can checkout the previous
    commit, but frankly, I probably won't even remember and I'll just start
    from scratch using Google. At least I won't get weird errors because of
    stale info in the config of a tool I haven't used in a while.

--- update.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-git pull
-# first do a recursive update of all submodules to pull in changes approved by third-party repositories
-git submodule update --recursive
-# then update toplevel submodules to the newest remote version
-git submodule update --remote --rebase
-git commit -am "update submodules"
-git push --recurse-submodules=on-demand
