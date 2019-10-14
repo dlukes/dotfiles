74bb45caa4928db5d61943a23bcfd346f7c35b0a
David Lukes <dafydd.lukes@gmail.com>
2019-10-14 11:10:41 +0200

    Update ELAN installer script
    ELAN FX is now the default, so the FX suffix has been dropped and the
    download links have changed.

--- elan_install.sh
+++ elan_install.sh
@@ -1,41 +1,42 @@
 #!/usr/bin/env zsh
 
-installer_url=$(
-  curl -s https://tla.mpi.nl/tools/tla-tools/elan/download/ |
-    grep -oPm1 'https?://.*?linux\.bin'
-)
-installer_basename=${installer_url:t}
+dot_local=$HOME/.local
 
-if [[ -z $installer_url ]]; then
+archive_url=$(
+  curl -s https://tla.mpi.nl/tools/tla-tools/elan/download/ |
+    grep -oiPm1 'https?://.*?_linux\.tar\.gz' |
+    head -n1
+)
+archive_basename=${archive_url:t}
+
+if [[ -z $archive_url ]]; then
   >&2 echo "Unable to determine download URL."
   exit 1
 fi
 
 # quit if this version is already installed
-version=$( echo $installer_basename | cut -f2 -d_ )
+version=$( echo $archive_basename | cut -f2 -d_ | cut -f1,2 -d- )
 major=${version%-*}
 minor=${version#*-}
-install_dir=$HOME/ELAN_$major.$minor
-if [[ -d $install_dir ]]; then
+unpack_dir=$dot_local/ELAN-$major.$minor
+if [[ -d $unpack_dir ]]; then
   >&2 echo "The latest ELAN version $major.$minor is already installed, only updating desktop launcher."
 else
-  temp_dir=$( mktemp -d )
-  cd $temp_dir
-
-  wget $installer_url
-  sh $installer_basename
+  mkdir -p $unpack_dir
+  cd $dot_local
+  wget $archive_url
+  tar xvzf $archive_basename -C $unpack_dir --strip-components 1 && rm -f $archive_basename
 fi
 
 # find the newest available ELAN executable
 elan_bin=$(
-  find $HOME -not -path '*/\.*' -type f -executable -name 'ELAN_*' -printf '%T@\t%p\n' |
-    grep -P '/ELAN_\d+\.\d+$' |
+  find $dot_local -type f -executable -name ELAN -printf '%T@\t%p\n' |
     sort -nr |
     head -n1 |
     cut -f2
 )
 
-cat <<EOF >~/.local/share/applications/ELAN.desktop
+cat <<EOF >$dot_local/share/applications/ELAN.desktop
 [Desktop Entry]
 Name=ELAN
 Exec=env "_JAVA_OPTIONS=-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Xmx2048m" "$elan_bin" %U
