d0755e3b8d7e0052d5acbf6b12747c87bc8175f6
David Lukes <dafydd.lukes@gmail.com>
2019-12-04 16:25:34 +0100

    Add R_install.sh

---
+++ R_install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env zsh
+
+function check_install_R_package {
+  R --quiet -e "require('$1') || install.packages('$1', repos = 'https://cloud.r-project.org')"
+}
+
+check_install_R_package languageserver
+check_install_R_package tidyverse
