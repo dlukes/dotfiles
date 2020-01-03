28798ca33d016d44a41d6039ef6f4d826b4fb53e
David Lukes <dafydd.lukes@gmail.com>
2020-01-03 16:47:50 +0100

    Use pyenv

--- python_install.sh
+++ python_install.sh
@@ -1,13 +1,39 @@
 #!/usr/bin/env zsh
 
-# Consider downloading pyenv and setting Python up that way, so that
-# it's consistent across machines.
+set -e
 
-# on macOS, I have brew, so no --user; if I ever end up configuring
-# pyenv across the board (cf. above), remove --user everywhere
-if ! uname | grep -q Darwin; then
-  user=--user
+if [[ $( uname ) == Darwin ]]; then
+  alias grep=ggrep
 fi
+
+export PYENV_ROOT=$HOME/.local/pyenv
+export PATH=$PYENV_ROOT/bin:$PATH
+if [[ -d $PYENV_ROOT ]]; then
+  git -C $PYENV_ROOT pull
+else
+  git clone https://github.com/pyenv/pyenv.git $PYENV_ROOT
+fi
+eval "$(pyenv init -)"
+
+py3_regex='^3\.\d+\.\d+$'
+curr_ver=$( pyenv versions --bare | grep -P $py3_regex | tail -n1 )
+new_ver=$(
+  pyenv install --list | tr -d ' ' | grep -P $py3_regex | tail -n1
+)
+if [[ $new_ver == $curr_ver ]]; then
+  >&2 echo "Most recent stable Python $curr_ver is already installed."
+else
+  if [[ ! -z $curr_ver ]]; then
+    >&2 echo "Uninstalling old Python $curr_ver"
+    pyenv uninstall $curr_ver
+  fi
+  >&2 echo "Installing most recent stable Python $new_ver"
+  pyenv install $new_ver
+fi
+
+pyenv global system
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python
+pyenv global $new_ver
 
 pip3 install $user --upgrade --upgrade-strategy eager \
   ipython \
@@ -34,5 +60,4 @@ pip3 install $user --upgrade --upgrade-strategy eager \
   pyls-black \
   \
   pip \
-  'poetry>=1.0.0a0' \
   pipenv
