ab7824b4817c55f8043dd706a1469616378465b7
David Lukes <dafydd.lukes@gmail.com>
2020-01-07 13:34:31 +0100

    Don't use uuidgen in nvim_install.sh
    Turns out it's not always available by default, and it's not really
    needed in practice.

--- nvim_install.sh
+++ nvim_install.sh
@@ -9,10 +9,9 @@ else # ------------------------------------------------------ START LINUX BRANCH
 
 # Warn if competing install of nvim found.
 
-not_found=$( uuidgen )
-on_path=$( command -v nvim || echo $not_found )
+on_path=$( command -v nvim || echo NVIM_NOT_FOUND )
 in_local=$HOME/.local/bin/nvim
-if [[ $on_path == $not_found ]]; then
+if [[ $on_path == NVIM_NOT_FOUND ]]; then
   echo "No previous nvim version found."
 elif [[ $on_path != $in_local ]]; then
   read -q "YN?You're currently using $on_path but installing $in_local, are you sure?"
@@ -20,8 +19,8 @@ fi
 
 # Compare versions and install / update / keep as is.
 
-if [[ $on_path == $not_found ]]; then
-  old_version=$not_found
+if [[ $on_path == NVIM_NOT_FOUND ]]; then
+  old_version=NVIM_NOT_FOUND
 else
   old_version=$( $on_path --version | head -n 1 | grep -oP '[\d\.]+' )
 fi
@@ -41,7 +40,7 @@ install() {
   ln -sf nvim.appimage $in_local
 }
 
-if [[ $old_version == $not_found ]]; then
+if [[ $old_version == NVIM_NOT_FOUND ]]; then
   install
   echo "Installed nvim $new_version."
 elif [[ $old_version != $new_version ]]; then
