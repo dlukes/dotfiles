75952b1fbd4990294edf9f5f1aca5cb095a818f7
David Lukes <dafydd.lukes@gmail.com>
2020-02-24 16:11:35 +0100

    Remove CSL styles from dotfiles
    Provide instead a script that will download them from zotero.org.

---
+++ bin/csl_install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+csl="$HOME/.csl"
+styles=(
+  apa
+  chicago-author-date
+  iso690-author-date-cs
+  unified-style-sheet-for-linguistics
+)
+
+mkdir -p "$csl"
+cd "$csl"
+for style in "${styles[@]}"; do
+  >&2 echo "Downloading style $style from zotero.org..."
+  curl -sLOJ https://www.zotero.org/styles/$style
+done
