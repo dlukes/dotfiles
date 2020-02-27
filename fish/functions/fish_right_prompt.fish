12d9b4f413667fbeb4d15fb96fa8b8dcb9c3be57
David Lukes <dafydd.lukes@gmail.com>
2020-02-27 11:42:15 +0100

    Use string join for right prompt
    Much easier to reason about than manually mucking about with separators
    when some of the elements of the prompt might be missing in various
    contexts.

--- fish/functions/fish_right_prompt.fish
+++ fish/functions/fish_right_prompt.fish
@@ -3,13 +3,12 @@ function fish_right_prompt --description 'Write out the right prompt'
   # status of the last interactively run command will be overwritten by
   # the previous function-internal command
   set last_pipestatus $pipestatus
-  set sep (set_color normal)' •'
 
   if test $CMD_DURATION -gt 3000
-    set duration '⏱  ' (set_color yellow)(date -ud @(math $CMD_DURATION / 1000) +%T)$sep
+    set duration '⏱  '(set_color yellow)(date -ud @(math $CMD_DURATION / 1000) +%T)
   end
 
-  set prompt_status (__fish_print_pipestatus ' ⚡' $sep '|' \
+  set prompt_status (__fish_print_pipestatus '⚡' '' '|' \
     (set_color $fish_color_status) \
     (set_color --bold $fish_color_status) \
     $last_pipestatus)
@@ -17,9 +16,9 @@ function fish_right_prompt --description 'Write out the right prompt'
   # check VCS info at most every .5s
   set now (date +%s%N)
   if not set -q _last_vcs_check || test (math $now - $_last_vcs_check) -gt 500000000
-    set -g _vcs (fish_vcs_prompt)
+    set -g _vcs (string trim (fish_vcs_prompt))
   end
   set -g _last_vcs_check (date +%s%N)
 
-  echo -ns $duration $prompt_status $_vcs
+  string join (set_color normal)' • ' $duration $prompt_status $_vcs
 end
