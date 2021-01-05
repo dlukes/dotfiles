d6040b4944e6a19d2bc8b9ec68e42fe8d8db2667
David Lukes <dafydd.lukes@gmail.com>
2021-01-05 13:12:08 +0100

    Make Node installation script smarter

--- bin/node_install.sh
+++ bin/node_install.sh
@@ -2,6 +2,50 @@
 
 set -euf
 
+
+
+# ---------------------------------------------------------- Node itself {{{1
+
+
+>&2 echo '>>> Installing Node...'
+
+install_node() {
+  prefix="$HOME"/.local
+  path=$(command -v node)
+  # check if a separate installation of Node exists somewhere outside
+  # the target prefix; if so, maybe we don't want to install Node after
+  # all
+  case "$path" in
+    "$prefix"*) ;;
+    *)
+      >&2 echo ">>> Detected Node outside target prefix, as $path."
+      read -ep ">>> Install Node under $prefix alongside this existing installation anyway? (y/n) " yn
+      case "$yn" in
+        Y*|y*) ;;
+        *) return ;;
+      esac
+      ;;
+  esac
+
+  version=lts
+  installed=$(node --version 2>/dev/null || echo none)
+  requested=$(curl -sL https://resolve-node.now.sh/$version)
+  if [ "$installed" != "$requested" ]; then
+    curl -sSfL install-node.now.sh/$version | bash -s -- --prefix="$prefix"
+  else
+    >&2 echo "Most recent $version version already installed: $installed."
+  fi
+}
+
+install_node
+
+
+
+# --------------------------------------------------------- NPM packages {{{1
+
+
+>&2 echo '>>> Installing NPM packages...'
+
 install_or_update() {
   if command -v "$1" >/dev/null; then
     cmd=update
@@ -10,13 +54,6 @@ install_or_update() {
   fi
   npm $cmd -g "$1"
 }
-
-version=lts
-installed=$(node --version 2>/dev/null || echo none)
-requested=$(curl -sL https://resolve-node.now.sh/$version)
-if [ "$installed" != "$requested" ]; then
-  curl -sSfL install-node.now.sh/$version | bash -s -- --prefix="$HOME/.local"
-fi
 
 install_or_update npm
 install_or_update pyright
