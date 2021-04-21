8df325bc009aa34c61db31a8d915f55272f1686a
David Lukes <dafydd.lukes@gmail.com>
2021-04-21 12:52:44 +0200

    Fasd jump also without tab complete

--- fish/functions/j.fish
+++ fish/functions/j.fish
@@ -1,5 +1,9 @@
 function j --description 'Jump to directory with fasd'
-  # NOTE: the fasd behavior is actually implemented by this function's
-  # tab completions
-  cd $argv
+  # NOTE: interactive selection is implemented in ../completions/j.fish
+  set -l dir (fasd -dlR $argv[1] | head -1)
+  if test -d "$dir"
+    cd $dir
+  else
+    cd $argv[1]
+  end
 end
