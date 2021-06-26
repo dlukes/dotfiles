9eeaefcb9545c7f68f4c213f143fe9ab5c10755d
David Lukes <dafydd.lukes@gmail.com>
2021-06-26 15:25:38 +0200

    Tweak pip_install.sh for macOS / M1

--- python/pip_install.sh
+++ python/pip_install.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 
-pip3 install --upgrade --upgrade-strategy eager \
+set -e
+dirname=$( dirname "$0" )
+. "$dirname/util.sh"
+
+if command -v brew &>/dev/null; then
+  brew_install_or_upgrade openblas
+  brew_install_or_upgrade libjpeg
+  export OPENBLAS=$(brew --prefix openblas)
+fi
+
+without_gnubin pip3 install --upgrade --upgrade-strategy eager \
   pip \
   wheel
-pip3 install --upgrade --upgrade-strategy eager \
+without_gnubin pip3 install --upgrade --upgrade-strategy eager \
   ipython \
   ipdb \
   scalene \
