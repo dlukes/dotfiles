4aa22c82a23b1354f1a0ce9b67e35ae1366e701c
David Lukes <dafydd.lukes@gmail.com>
2021-08-11 15:47:12 +0200

    Revamp Doom installation script
    With recent versions of Emacs and Doom, all the config should be able to
    go under XDG_CONFIG_HOME. So let's not clutter the home directory.

---
+++ bin/doom_install.sh
@@ -0,0 +1,1 @@
+../emacs/doom_install.sh
---
+++ emacs/doom_install.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+set -e
+script_dir=$(dirname "$(realpath "$0")")
+
+if [ -z "$XDG_CONFIG_HOME" ]; then
+  export XDG_CONFIG_HOME="$HOME/.config"
+fi
+
+# This is one of the *standard* DOOMDIR locations expected by Doom. If
+# XDG_CONFIG_HOME is not set, Doom should default to ~/.config, so
+# everything should work fine even without symlinking this to ~/.doom.d
+# (the other standard location for DOOMDIR, which doesn't depend on
+# XDG_CONFIG_HOME, but clutters the home directory).
+export DOOMDIR="$XDG_CONFIG_HOME/doom"
+if [ "$(realpath "$DOOMDIR")" != "$script_dir/doom" ]; then
+  echo debug "$(realpath "$DOOMDIR")"
+  echo debug "$script_dir/doom"
+  >&2 echo "Private Doom config not in place, run symlink.sh first."
+  exit 1
+fi
+
+# Ditto for this location vs. ~/.emacs.d, except this depends on fairly
+# recent support for XDG_CONFIG_HOME in Emacs itself. According to
+# Emacs's etc/NEWS.27 file, it looks like XDG_CONFIG_HOME will also
+# fall back to ~/.config if not set -- let's hope so.
+#
+# Ideally, if you want to test a vanilla Emacs config, just create an
+# empty ~/.emacs.d and off you go.
+emacs_d="$XDG_CONFIG_HOME/emacs"
+
+if command -v doom 2>&2; then
+  doom upgrade  # = cd ~/.emacs.d; git pull; doom clean, sync, update
+else
+  mkdir -p "$XDG_CONFIG_HOME"
+  git clone https://github.com/hlissner/doom-emacs "$emacs_d"
+  "$emacs_d"/bin/doom install
+  ln -sft ~/.local/bin "$emacs_d"/bin/doom
+fi
