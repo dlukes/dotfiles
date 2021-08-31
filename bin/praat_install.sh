a332c246fd1e936dc4a7854324f3d98eda212b24
David Lukes <dafydd.lukes@gmail.com>
2021-09-01 00:05:34 +0200

    Add Praat install script

---
+++ bin/praat_install.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# exit on error; no unset variables; no pathname expansion; trace execution
+set -euf
+script_dir=$(dirname "$0")
+. "$script_dir"/util.sh
+
+if is_macos; then
+  >&2 echo "On macOS, install manually from <https://www.fon.hum.uva.nl/praat/download_mac.html>."
+  exit 1
+fi
+
+>&2 echo "Current version: $(praat --version)"
+
+base_url=https://www.fon.hum.uva.nl/praat
+latest_version_archive=$(
+  curl -sSfL "$base_url"/download_linux.html |
+    grep -oPm1 'praat.*?_linux64.tar.gz' |
+    head -1
+)
+download_url="$base_url/$latest_version_archive"
+
+cd "$HOME"/.local/bin
+curl -sSfLO "$download_url"
+tar xzf "$latest_version_archive"
+rm "$latest_version_archive"
+
+>&2 echo "Newly installed version: $(praat --version)"
