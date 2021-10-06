af3ec36b983dd4c2e93e85381c7552d455d0b45b
David Lukes <dafydd.lukes@gmail.com>
2021-10-06 21:56:31 +0200

    Python: UMRK type and docstring tweaks

--- python/umrk/src/umrk/stats.py
+++ python/umrk/src/umrk/stats.py
@@ -1,22 +1,24 @@
 """Various association measures."""
 
 import math
-from numbers import Real
 from typing import Optional, Union, Sequence
 
 from scipy.stats import entropy as _entropy
 
 
 def entropy(
-    dist: Sequence[Real],
-    other_dist: Optional[Sequence[Real]] = None,
+    dist: Sequence[float],
+    other_dist: Optional[Sequence[float]] = None,
     base: Union[int, float] = 2,
     axis: int = 0,
 ) -> float:
     """Entropy of dist, or relative entropy w.r.t. other_dist.
 
     Relative entropy is also called Kullback-Leibler divergence and measures how much
-    one probability distribution differs from another.
+    one probability distribution differs from another. **In many cases, this is probably
+    what you want in corpus applications**, to make comparisons possible between common
+    and less common types. A good default for other_dist is a uniform distribution with
+    the same number of items as dist, i.e. `np.ones(len(dist))`.
 
     See docstring of scipy.stats.entropy for more details.
 
@@ -24,12 +26,12 @@ def entropy(
     return _entropy(dist, other_dist, base, axis)
 
 
-def mi(px: Real, py: Real, pxy: Real) -> float:
+def mi(px: float, py: float, pxy: float) -> float:
     """Mutual information between events x and y."""
     return math.log2(pxy / px / py)
 
 
-def mi_score(fx: Real, fy: Real, fxy: Real, N: Real) -> float:
+def mi_score(fx: float, fy: float, fxy: float, N: float) -> float:
     """MI-score between corpus types x and y.
 
     Arguments: frequency of x, y, and the co-occurrence of x and y, and corpus size.
@@ -41,15 +43,15 @@ def mi_score(fx: Real, fy: Real, fxy: Real, N: Real) -> float:
     return math.log2(N * fxy / fx / fy)
 
 
-def t_score(fx: Real, fy: Real, fxy: Real, N: Real) -> float:
+def t_score(fx: float, fy: float, fxy: float, N: float) -> float:
     return (fxy - fx * fy / N) / math.sqrt(fxy)
 
 
-def dice(fx: Real, fy: Real, fxy: Real) -> float:
+def dice(fx: float, fy: float, fxy: float) -> float:
     return 2 * fxy / (fx + fy)
 
 
-def log_dice(fx: Real, fy: Real, fxy: Real) -> float:
+def log_dice(fx: float, fy: float, fxy: float) -> float:
     return 14 + math.log2(dice(fx, fy, fxy))
 
 
