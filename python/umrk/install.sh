203932bd9012dcee7c860e99c1dff367ba3f8d61
David Lukes <dafydd.lukes@gmail.com>
2022-01-02 21:59:53 +0100

    UMRK: Use plain setuptools, relax deps

--- python/umrk/install.sh
+++ python/umrk/install.sh
@@ -6,7 +6,9 @@ if python -c "
 import sys
 from pathlib import Path
 for dir in sys.path:
-  if (Path(dir) / 'umrk.egg-link').is_file():
+  p = Path(dir) / 'umrk.egg-link'
+  if p.is_file():
+    print(f'Found egg-link: {p}', file=sys.stderr)
     sys.exit(0)
 sys.exit(1)
 "; then
@@ -17,15 +19,4 @@ fi
 >&2 echo "Installing UMRK in editable mode."
 script_dir=$(dirname "$(realpath "$0")")
 cd "$script_dir"
-
-# generate a setup.py
-poetry build
-tar xzOf dist/umrk-0.1.0.tar.gz --no-anchored setup.py >setup.py
-rm -rf dist
-
-# temporarily hide the pyproject.toml; required because of some bug in setuptools or
-# pip, otherwise the editable install falls over
-mv pyproject.toml pyproject.toml:disabled
-pip install -e .
-mv pyproject.toml:disabled pyproject.toml
-rm setup.py
+python3 -m pip install -e .
