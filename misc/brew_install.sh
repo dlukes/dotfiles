9f617322e087e4f3fbc5f0aa6b2afcf1bc0d7535
David Lukes <dafydd.lukes@gmail.com>
2022-02-25 12:36:21 +0100

    Brew: Add tidy-html5
    I don't particularly love/need it, but macOS ships with a *really* old
    version of tiny, which various other tools might pick up by default,
    wreaking all kinds of havoc. So better install a newer version by
    default to override it.

--- misc/brew_install.sh
+++ misc/brew_install.sh
@@ -2,7 +2,11 @@
 
 set -euf
 
+# tidy-html5 is because macOS ships with an obsolete version of tidy, which other tools
+# (like Doom Emacs) might silently pick up and use, corrupting HTML and XML files left
+# and right.
 brew install \
   fish git \
   coreutils findutils gnu-sed gnu-tar gawk \
-  gcc readline openssl
+  gcc readline openssl \
+  tidy-html5
