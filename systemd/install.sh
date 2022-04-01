63fb361a58c01ac32ff53840493a336abe554800
David Lukes <dafydd.lukes@gmail.com>
2022-04-01 17:47:14 +0200

    Systemd: Journald conf, cp instead of ln

--- systemd/install.sh
+++ systemd/install.sh
@@ -1,14 +1,30 @@
 #!/bin/sh
 
+# NOTE: With SELinux enabled, symlinking system-wide config files to your home dir is...
+# tricky. It might sometimes work if you chcon appropriately, but sometimes even that is
+# not enough and the system process won't be allowed to read your home dir. So better
+# just copy them. At least, it looks like you don't have to futz around with chcon, yay.
+
 set -eu
 script_dir=$(dirname "$(realpath "$0")")
 cd "$script_dir"
 
 log() {
   { printf -- "-%.s" $(seq $(tput cols)); echo; } >&2
-  >&2 echo Configuring unit file "$1"
+  >&2 echo Configuring "$1"
   { printf -- "-%.s" $(seq $(tput cols)); echo; } >&2
 }
+
+
+
+# ---------------------------------------------------------------------- Journald config {{{1
+
+
+journald_conf_d=/etc/systemd/journald.conf.d
+log "$journald_conf_d"
+sudo mkdir -p "$journald_conf_d"
+sudo cp -t "$journald_conf_d" journald.conf.d/*
+sudo systemctl restart systemd-journald
 
 
 
@@ -28,9 +44,8 @@ done
 
 for unit in "$PWD"/system/*; do
   log "$unit"
-  # The appropriate SELinux context can be figured out by running ls -Z on a sample
-  # distro-provided Systemd unit file.
-  chcon system_u:object_r:systemd_unit_file_t:s0 "$unit"
-  sudo systemctl --no-block enable --force "$unit"
-  sudo systemctl --no-block restart "$(basename "$unit")"
+  sudo cp -t /etc/systemd/system "$unit"
+  unit=$(basename "$unit")
+  sudo systemctl --no-block enable --force $unit
+  sudo systemctl --no-block restart $unit
 done
