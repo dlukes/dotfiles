dd2ad4f3399f0a6cd33877c2c1b56750f9d50b80
David Lukes <dafydd.lukes@gmail.com>
2022-04-01 14:40:30 +0200

    Systemd: Better install script

--- systemd/install.sh
+++ systemd/install.sh
@@ -4,13 +4,21 @@ set -eu
 script_dir=$(dirname "$(realpath "$0")")
 cd "$script_dir"
 
+log() {
+  { printf -- "-%.s" $(seq $(tput cols)); echo; } >&2
+  >&2 echo Configuring unit file "$1"
+  { printf -- "-%.s" $(seq $(tput cols)); echo; } >&2
+}
+
 
 
 # ------------------------------------------------------------------------ User services {{{1
 
 
 for unit in "$PWD"/user/*; do
-  systemctl --user enable --force --now "$unit"
+  log "$unit"
+  systemctl --no-block --user enable --force "$unit"
+  systemctl --no-block --user restart "$(basename "$unit")"
 done
 
 
@@ -19,8 +27,10 @@ done
 
 
 for unit in "$PWD"/system/*; do
+  log "$unit"
   # The appropriate SELinux context can be figured out by running ls -Z on a sample
   # distro-provided Systemd unit file.
   chcon system_u:object_r:systemd_unit_file_t:s0 "$unit"
-  sudo systemctl enable --force --now "$unit"
+  sudo systemctl --no-block enable --force "$unit"
+  sudo systemctl --no-block restart "$(basename "$unit")"
 done
