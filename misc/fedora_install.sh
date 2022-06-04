572c754d57b6b77df3782d227f3ffa1a2b18da81
David Lukes <dafydd.lukes@gmail.com>
2022-06-04 14:02:37 +0200

    Fedora: Install media codecs

--- misc/fedora_install.sh
+++ misc/fedora_install.sh
@@ -1,9 +1,33 @@
 #!/bin/sh
 
+set -euf
+
 # Stuff you might want to pull in on every Fedora install to make your life easier.
 
+
+
+# ------------------------------------------------------ Install/enable additional repos {{{1
+
+
+sudo dnf in \
+  https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+  https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf config-manager --set-enabled fedora-cisco-openh264 google-chrome
+
+
+
+# ----------------------------------------------------------------- Software compilation {{{1
+
+
 sudo dnf grp install -by "Development Tools" "Development Libraries"
-sudo dnf in -by libstdc++-devel \
+sudo dnf in -by libstdc++-devel
+
+
+
+# --------------------------------------------------------------------- GNOME extensions {{{1
+
+
+sudo dnf in -by \
   gnome-shell-extension-appindicator \
   gnome-shell-extension-caffeine \
   gnome-shell-extension-freon \
@@ -18,3 +42,23 @@ sudo dnf in -by libstdc++-devel \
 #   environment (you probably don't, you're used to GNOME Shell as it is)
 # - gnome-shell-extension-netspeed for a dedicated network speed indicator, but the
 #   system monitor applet has this too and it looks less distracting
+
+
+
+# ----------------------------------------------------------------------- Media playback {{{1
+
+
+# Just following the guidelines from the quick docs:
+#
+# - https://docs.fedoraproject.org/en-US/quick-docs/assembly_installing-plugins-for-playing-movies-and-music/
+# - https://docs.fedoraproject.org/en-US/quick-docs/openh264/
+
+sudo dnf in -by \
+  gstreamer1-plugins-{bad-\*,good-\*,base} \
+  gstreamer1-plugin-openh264 \
+  gstreamer1-libav \
+  --exclude=gstreamer1-plugins-bad-free-devel \
+  lame\* \
+  --exclude=lame-devel \
+  mozilla-openh264
+sudo dnf grp install -by --with-optional Multimedia
