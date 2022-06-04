6cd59c161c9e74e5eb6c1a8e00aa608301e0b7b6
David Lukes <dafydd.lukes@gmail.com>
2022-06-04 13:38:56 +0200

    Fedora: More GNOME extensions

--- misc/fedora_install.sh
+++ misc/fedora_install.sh
@@ -4,4 +4,17 @@
 
 sudo dnf grp install -by "Development Tools" "Development Libraries"
 sudo dnf in -by libstdc++-devel \
-  gnome-shell-extension-appindicator
+  gnome-shell-extension-appindicator \
+  gnome-shell-extension-caffeine \
+  gnome-shell-extension-freon \
+  gnome-shell-extension-gamemode \
+  gnome-shell-extension-openweather \
+  gnome-shell-extension-sound-output-device-chooser \
+  gnome-shell-extension-system-monitor-applet
+
+# Suggestions:
+#
+# - gnome-shell-extension-pop-shell if you want a more sophisticated tiling window
+#   environment (you probably don't, you're used to GNOME Shell as it is)
+# - gnome-shell-extension-netspeed for a dedicated network speed indicator, but the
+#   system monitor applet has this too and it looks less distracting
