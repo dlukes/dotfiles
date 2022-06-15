5670bc78fa1a7a44ecc2bba1d6071fee4a8d7298
David Lukes <dafydd.lukes@gmail.com>
2022-06-15 12:01:43 +0200

    Praat: Create desktop file

--- misc/praat_install.sh
+++ misc/praat_install.sh
@@ -11,6 +11,7 @@ fi
 
 >&2 echo "Current version: $(praat --version 2>/dev/null || echo none)"
 
+prefix="$HOME"/.local
 base_url=https://www.fon.hum.uva.nl/praat
 latest_version_archive=$(
   curlk -sSfL "$base_url"/download_linux.html |
@@ -19,9 +20,24 @@ latest_version_archive=$(
 )
 download_url="$base_url/$latest_version_archive"
 
-cd "$HOME"/.local/bin
+cd "$prefix"/bin
 curlk -sSfLO "$download_url"
 tar xzf "$latest_version_archive"
 rm "$latest_version_archive"
+
+cat <<'EOF' >"$prefix"/share/applications/praat.desktop
+[Desktop Entry]
+Name=Praat
+GenericName=Sound Editor
+Comment=Doing phonetics by computer
+Icon=audacity
+Type=Application
+Categories=AudioVideo;Audio;AudioVideoEditing;
+# %F or %U?
+Exec=praat --hide-picture --open %F
+StartupNotify=false
+Terminal=false
+# MimeType=application/x-audacity-project;audio/flac;audio/x-flac;audio/mpeg;application/ogg;audio/x-vorbis+ogg;
+EOF
 
 >&2 echo "Newly installed version: $(praat --version)"
