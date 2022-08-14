9c7bc8a4a11d60730b3fd08ed37a922533d27596
David Lukes <dafydd.lukes@gmail.com>
2022-08-14 13:38:49 +0200

    Mamba: Revamp install

--- mamba/install.sh
+++ mamba/install.sh
@@ -2,25 +2,35 @@
 
 set -euf
 script_dir=$(dirname "$(realpath "$0")")
-prefix="$HOME"/.local/mambaforge
-installer="Mambaforge-$(uname)-$(uname -m).sh"
+. "$script_dir"/../misc/util.sh
 
+prefix="$HOME"/.local/mambaforge
+conda_config="$prefix"/etc/profile.d/conda.sh
+installer="Mambaforge-$(uname)-$(uname -m).sh"
+default_env=umrk
+env_file="$script_dir"/$default_env.yml
+
+info 'Unsetting Conda-related environment variables, if any.'
+for var in $(env | grep CONDA); do
+  var="${var%=*}"
+  >&2 echo "Unsetting $var."
+  unset "$var"
+done
+
+info "Downloading and installing $installer."
 curl -sSfLO https://github.com/conda-forge/miniforge/releases/latest/download/"$installer"
-(
-  unset CONDA_PREFIX PYTHONPATH
-  bash "$installer" -bup "$prefix"
-)
+bash "$installer" -bup "$prefix"
 rm "$installer"
 
-export PATH="$prefix/bin:$PATH"
-mamba env update --name umrk --file "$script_dir"/umrk.yml
-. "$prefix"/etc/profile.d/conda.sh
-conda activate umrk
+info "Setting up and activating default environment $default_env from $env_file."
+. "$conda_config"
+mamba env update --name $default_env --file "$env_file"
+conda activate $default_env
 
->&2 echo ">>> Running additional Pip installs with $(command -v python3)."
+info "Installing UMRK Python package for $(command -v python3)."
 "$script_dir"/../python/umrk/install.sh
 
->&2 echo ">>> Running additional NPM installs with $(command -v npm)."
+info "Running additional NPM installs with $(command -v npm)."
 "$script_dir"/../misc/npm_install.sh
 
->&2 echo '>>> Default environment is umrk. Remember that environments can be stacked.'
+info "Default environment is $default_env. Remember that environments can be stacked."
