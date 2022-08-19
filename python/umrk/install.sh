652c433a97e0197ded16aaa8fe4ff5530d8b8c1f
David Lukes <dafydd.lukes@gmail.com>
2022-08-19 11:49:03 +0200

    UMRK: Simplify install.sh

--- python/umrk/install.sh
+++ python/umrk/install.sh
@@ -2,21 +2,11 @@
 
 set -euf
 
-if python -c "
-import sys
-from pathlib import Path
-for dir in sys.path:
-  p = Path(dir) / 'umrk.egg-link'
-  if p.is_file():
-    print(f'Found egg-link: {p}', file=sys.stderr)
-    sys.exit(0)
-sys.exit(1)
-"; then
-  >&2 echo "UMRK is already installed in editable mode."
+if python3 -c 'import umrk' >/dev/null 2>&1; then
+  >&2 echo "UMRK is already installed."
   exit
 fi
 
->&2 echo "Installing UMRK in editable mode."
 script_dir=$(dirname "$(realpath "$0")")
 cd "$script_dir"
 python3 -m pip install -e .
