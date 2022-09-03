71054876b3bdf9d3c26f2690f10ac4eb3fcd57cd
David Lukes <dafydd.lukes@gmail.com>
2022-09-03 14:21:30 +0200

    Doom: Remove manual parinfer-rust compilation

--- emacs/install.sh
+++ emacs/install.sh
@@ -70,24 +70,6 @@ else
   ln -sft ~/.local/bin "$doom"
 fi
 
-# Until https://github.com/justinbarclay/parinfer-rust-mode/issues/43 is resolved,
-# compile the parinfer-rust dylib manually on macOS because of lacking M1 support.
-parinfer_so=$(find "$emacs_d" -name parinfer-rust-darwin.so)
-if [ -f "$parinfer_so" ]; then
-  >&2 echo '>>> Building parinfer-rust manually to make sure it has M1 support.'
-  cd ~/.cache
-  repo_name=parinfer-rust
-  if ! [ -d "$repo_name" ]; then
-    git clone --depth 1 https://github.com/eraserhd/"$repo_name".git
-    cd "$repo_name"
-  else
-    cd "$repo_name"
-    git fetch
-  fi
-  cargo build --release --features emacs
-  cp target/release/libparinfer_rust.dylib "$parinfer_so"
-fi
-
 >&2 echo "\
 >>> All done. Remember you might need to run 'doom build' to recompile
 installed packages in certain situations:
