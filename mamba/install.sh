db11ee259eedace08da4a4ed1ae3d91d93aca4ab
David Lukes <dafydd.lukes@gmail.com>
2022-09-21 13:11:35 +0200

    R: Linux vs macOS install

--- mamba/install.sh
+++ mamba/install.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
-set -euf
+set -eufo pipefail
 script_dir=$(dirname "$(realpath "$0")")
 . "$script_dir"/../misc/util.sh
 
@@ -8,8 +8,6 @@ prefix="$HOME"/.local/mambaforge
 conda_config="$prefix"/etc/profile.d/conda.sh
 installer="Mambaforge-$(uname)-$(uname -m).sh"
 default_env=umrk
-env_file="$script_dir"/$default_env.yml
-r_env_file="$script_dir"/r.yml
 
 info 'Unsetting Conda-related environment variables, if any.'
 for var in $(env | grep CONDA); do
@@ -23,6 +21,7 @@ curl -sSfLO https://github.com/conda-forge/miniforge/releases/latest/download/"$
 bash "$installer" -bup "$prefix"
 rm "$installer"
 
+env_file="$script_dir"/$default_env.yml
 info "Setting up and activating default environment $default_env from $env_file."
 . "$conda_config"
 mamba env update --name $default_env --file "$env_file"
@@ -34,8 +33,11 @@ info "Installing UMRK Python package for $(command -v python3)."
 info "Running additional NPM installs with $(command -v npm)."
 "$script_dir"/../misc/npm_install.sh
 
-info "Installing R and packages from $r_env_file."
-mamba env update --name $default_env --file "$r_env_file"
+r_env_files=(r-common.yml r-$(uname -s).yml)
+for r_env_file in "${r_env_files[@]}"; do
+  info "Installing R and packages from $r_env_file."
+  mamba env update --name $default_env --file "$script_dir/$r_env_file"
+done
 info "Installing IRkernel kernelspec with $(command -v R)."
 R --quiet -e "IRkernel::installspec()"
 
