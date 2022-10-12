7104794798725f481bd5cffc9a3d3c8e7d98b4e1
David Lukes <dafydd.lukes@gmail.com>
2022-10-13 00:14:47 +0200

    Doom: More Python-related functionality

--- emacs/doom/init.el
+++ emacs/doom/init.el
@@ -176,7 +176,7 @@
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
-       (python +tree-sitter)            ; beautiful is better than ugly
+       (python +conda +cython +lsp +pyright +tree-sitter)            ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
