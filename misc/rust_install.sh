f9b789f3d42b45fbdbdb30aa96ae38603a603228
David Lukes <dafydd.lukes@gmail.com>
2023-02-10 03:45:02 +0100

    Rust: Mention cargo-binstall

--- misc/rust_install.sh
+++ misc/rust_install.sh
@@ -9,23 +9,35 @@ comp_dir="$HOME/.config/fish/completions"
 registry="$HOME/.cargo/registry"
 mkdir -p "$man_dir" "$comp_dir"
 
->&2 echo '>>> HINT: Any options are passed on to the cargo install calls (e.g. --force).'
+if [ -n "${CONDA_PREFIX:-}" ];then
+  error "\
+Detected Conda env: $CONDA_PREFIX
+If it contains a compiler toolchain, it might interfere with Rust compilation. Better
+de-activate it first."
+  exit 1
+fi
 
-### Rustup
+
+
+# ------------------------------------------------------------------------------- Rustup {{{1
+
 
 if ! command -v rustup >/dev/null 2>&1; then
-  >&2 echo '>>> Installing rustup...'
+  info 'Installing rustup...'
   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 else
-  >&2 echo '>>> Updating rustup...'
+  info 'Updating rustup...'
   rustup update
 fi
 
 rustup completions fish >"$comp_dir/rustup.fish"
 
-### Rust analyzer
 
->&2 echo '>>> Installing rust-analyzer...'
+
+# ------------------------------------------------------------------------ Rust analyzer {{{1
+
+
+info 'Installing rust-analyzer...'
 
 if is_macos; then
   suffix=aarch64-apple-darwin
@@ -42,9 +54,26 @@ gunzip $rust_analyzer_gz
 chmod +x $rust_analyzer
 mv $rust_analyzer ~/.local/bin/rust-analyzer
 
-### Utils
 
->&2 echo '>>> Installing general Rust-based utils...'
+
+# ----------------------------------------------------------------- TODO: Cargo-binstall {{{1
+# Can be used to install binaries (if available) instead of compiling from source. As of
+# 2023-02-10, I've found it to be unreliable and slow for the tools I use. For both
+# ripgrep and fd-find, it has to fall back to QuickInstall, and takes its sweet time to
+# do so. Also, for ripgrep, the pre-compiled builds will probably be without PCRE2, but
+# I should be able to live with that in exchange for faster installation. So keep an eye
+# on the project and possibly use it as the default installation method in the future.
+
+
+# info 'Installing cargo-binstall...'
+# cargo install --locked cargo-binstall
+
+
+
+# -------------------------------------------------------------------------------- Utils {{{1
+
+
+info 'Installing general Rust-based utils...'
 
 # Copy asset (typically man page or completions) not automatically
 # installed by cargo.
@@ -86,21 +115,27 @@ utils=(
 for util in ${utils[@]}; do
   tmp=$( mktemp -d )
   util=$( echo "$util" | sed -e 's/+/ /g')
-  CARGO_TARGET_DIR="$tmp" cargo install --locked "$@" $util
+  CARGO_TARGET_DIR="$tmp" cargo install --locked $util
   copy_asset $util "$tmp" "$man_dir" -regextype egrep -regex ".*/$util-.*/[[:alnum:]]+\.1(\.gz)?"
   copy_asset $util "$tmp" "$comp_dir" -path "*/$util-*" -name "*.fish"
 done
 
-### Cargo extensions
 
->&2 echo '>>> Installing cargo extensions...'
+
+# --------------------------------------------------------------------- Cargo extensions {{{1
+
+
+info 'Installing cargo extensions...'
 
 extensions=(
   # keeping track (and pruning) the cache under ~/.cargo
   cargo-cache
-  # updating global binaries (NOTE: the command is cargo install-update)
+  # updating global binaries (NOTE: the command is cargo install-update); should
+  # automatically use cargo-binstall if available
   cargo-update
 )
-cargo install --locked "$@" ${extensions[@]}
+cargo install --locked ${extensions[@]}
 
->&2 echo '>>> All done. Consult your personal notes on Rustup, Cargo and Rust for maintenance tips.'
+info 'All done. Consult your Org-roam on Rustup, Cargo and Rust for maintenance tips.'
+
+# vi: foldmethod=marker
