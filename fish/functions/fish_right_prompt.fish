60d8d7e1fdb907a103e4174ec3204ddb17b789e7
David Lukes <dafydd.lukes@gmail.com>
2023-03-10 13:06:50 +0100

    Fish: Warn if *any* command in pipeline fails
    Instead of warning only if *last* command in pipeline fails.

--- fish/functions/fish_right_prompt.fish
+++ fish/functions/fish_right_prompt.fish
@@ -8,11 +8,21 @@ function fish_right_prompt --description 'Write out the right prompt'
     set duration '⏰ '(set_color yellow)(date -ud @(math $CMD_DURATION / 1000) +%T)
   end
 
-  set prompt_status (__fish_print_pipestatus '⚡' '' '|' \
   # NOTE: If status reporting stops working, the culprit is probably a fish_right_prompt
   # wrapper that fails to preserve $pipestatus, e.g. from Conda. Check if `type
   # fish_right_prompt` shows the present function, and if not, figure out how to get rid
   # of the wrapper, or make it preserve $pipestatus.
+  #
+  # By default, __fisht_print_pipestatus only prints if the *last* status was an error
+  # (= anything other than code 0 or 141, see `type __fish_print_pipestatus`). However,
+  # I'd rather see a warning whenever *any* status in the last pipe was an error. This
+  # can be achieved by explicitly setting __fish_last_status and exporting it, see:
+  # https://github.com/fish-shell/fish-shell/blob/5197bf75cde5934ebd5b44a97176effb03e89fd9/share/functions/fish_prompt.fish#L6
+  set errors (string match -rv '^(?:0|141)$' -- $last_pipestatus)
+  if test -n "$errors"
+    set -x __fish_last_status $errors[1]
+  end
+  set prompt_status (__fish_print_pipestatus '⚡[' ']' '|' \
     (set_color $fish_color_status) \
     (set_color --bold $fish_color_status) \
     $last_pipestatus)
