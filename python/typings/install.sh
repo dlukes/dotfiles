3d6f277a3664586c45bfb025946791e56cf6f033
David Lukes <dafydd.lukes@gmail.com>
2023-03-20 16:54:59 +0100

    Handle possibly dirty Python typings repo

--- python/typings/install.sh
+++ python/typings/install.sh
@@ -5,7 +5,14 @@ cd "$HOME"/.local/share
 repo=python-type-stubs
 if [ -d "$repo" ]; then
   cd "$repo"
+  dirty=$(git status --porcelain)
+  if [ -n "$dirty" ]; then
+    git stash
+  fi
   git pull
+  if [ -n "$dirty" ]; then
+    git stash pop
+  fi
 else
   git clone git@github.com:microsoft/"$repo".git
 fi
