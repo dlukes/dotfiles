1c1b6165a3ae715ce8b7352d753b7ba052031b43
David Lukes <dafydd.lukes@gmail.com>
2023-04-27 11:25:05 +0200

    Mamba: Add conda-lock subcommand to base env

--- mamba/install.sh
+++ mamba/install.sh
@@ -28,6 +28,9 @@ mamba env update --name $default_env --file "$env_file"
 conda activate $default_env
 get typings
 
+info 'Installing conda-lock extension into base environment.'
+mamba install --channel=conda-forge --name=base conda-lock
+
 info "Installing UMRK Python package for $(command -v python3)."
 "$script_dir"/../python/umrk/install.sh
 
