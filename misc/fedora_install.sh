42cfed28dc95174dd82fa86a789b78f92bceb1b7
David Lukes <dafydd.lukes@gmail.com>
2023-09-14 17:09:37 +0200

    Fedora: Add Emacs

--- misc/fedora_install.sh
+++ misc/fedora_install.sh
@@ -81,7 +81,7 @@ sudo dnf grp install -by --with-optional Multimedia
 
 
 sudo dnf in -by \
-  vim fish ffmpeg sox vlc \
+  vim emacs fish ffmpeg sox vlc \
   aspell aspell-en aspell-cs aspell-fr aspell-es \
   tilix tilix-nautilus \
   nextcloud-client nextcloud-client-nautilus
